15e29e8ce9408a2d8902a63c37855617b1540e15
angel400567-glitch <angel400567@gmail.com>
2025-08-19 16:09:17 -0600

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author dylan
+ */
+public class Person {
+    
+    
+    //Atributes 
+    private String name;
+    private String lastname;
+    private String id;
+    private String phone;
+    private int age;
+
+    public Person(String name, String lastname, String id, String phone, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person\n"
+                + "" + "\nname=" + getName() 
+                + "\n, lastName=" + getLastname()
+                + "\n, id=" + getId() 
+                + "\n, phone=" + getPhone()
+                + "\n, age=" + getAge();
+        
+    
+    
+    
+    }
+ }
+    
