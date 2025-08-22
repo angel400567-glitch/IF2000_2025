125e414197660ead07091d03b61cc9e7383951fa
angel400567-glitch <angel400567@gmail.com>
2025-08-21 19:23:12 -0600

    Primer commit del proyecto IF2000_2025

---
+++ IF2000_introduccionProgra/src/main/java/IF2000_introduccionprogra/IF2000_introduccionProgra.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package IF2000_introduccionProgra;
+
+/**
+ *
+ * @author dylan
+ */
+public class IF2000_introduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Data types
+        String name = "Lionel";
+        int age = 40;
+        
+        System.out.println ("Hello World");
+        System.out.println ("Name:"+ name);
+        System.out.println ("Age:"+ age);
+        
+        //Python:
+        //print('Hola Mundo')
+        
+        /*Tipos de datos numéricos
+        int, long, short, float, double, numeric, money, decimal
+        
+        Manejo de flechas:
+        Date
+        
+        Cadenass de caracteres:
+        String, char
+        
+        Obejectos:
+        Object
+        
+        Data Structures:
+        Arrays or Vectors: int[] x;
+                           int[][] y;
+        
+        
+        True/False:
+        boolean
+        
+        */
+        
+    }
+    
+}
